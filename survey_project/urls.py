62c9eb5f08bf27a6129680de5ddc8906f4a65cea
BenedictusAryo <benedictusaryo@outlook.com>
2025-10-02 17:26:19 +0700

    Enhance home page: implement HomeView for dynamic statistics display, update template to show total datasets, forms, and responses for authenticated users

--- survey_project/urls.py
+++ survey_project/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('accounts/', include('accounts.urls')),
     path('master-data/', include('master_data.urls')),
     path('forms/', include('forms.urls')),
---
+++ survey_project/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import TemplateView
+from forms.models import Form
+from master_data.models import MasterDataset
+from responses.models import Response
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        if self.request.user.is_authenticated and hasattr(self.request.user, 'is_form_creator') and self.request.user.is_form_creator:
+            # Get statistics for form creators
+            context['total_datasets'] = MasterDataset.objects.filter(created_by=self.request.user).count()
+            context['total_forms'] = Form.objects.filter(created_by=self.request.user).count()
+            context['total_responses'] = Response.objects.filter(form__created_by=self.request.user).count()
+        
+        return context
