70f6f63d586397ae456ec9205ddf56a7965db08b
BenedictusAryo <benedictusaryo@outlook.com>
2025-10-03 14:33:35 +0700

    Add urlize_target_blank filter to convert URLs to clickable links in templates

--- forms/templatetags/form_extras.py
+++ forms/templatetags/form_extras.py
@@ -1,5 +1,7 @@
 from django import template
+from django.utils.safestring import mark_safe
 import json
+import re
 
 register = template.Library()
 
@@ -17,3 +19,26 @@ def record_display(record, attachment):
 def json_script(value):
     """Safely convert Python data to JavaScript JSON."""
     return json.dumps(value)
+
+@register.filter
+def urlize_target_blank(text):
+    """
+    Convert URLs in text to clickable links that open in a new tab.
+    This ensures the form stays open when users click external links.
+    """
+    if not text:
+        return text
+    
+    # Regular expression to match URLs
+    url_pattern = re.compile(
+        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    )
+    
+    def replace_url(match):
+        url = match.group(0)
+        return f'<a href="{url}" target="_blank" rel="noopener noreferrer" class="text-primary underline hover:text-primary-focus transition-colors">{url}</a>'
+    
+    # Replace URLs with clickable links
+    text_with_links = url_pattern.sub(replace_url, text)
+    
+    return mark_safe(text_with_links)
