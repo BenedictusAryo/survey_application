24fe315c92a30669ad5ce2f2c3d23efd7e712f27
BenedictusAryo <benedictusaryo@outlook.com>
2025-10-04 16:16:57 +0700

    fix mysql utf8

--- survey_project/settings_production.py
+++ survey_project/settings_production.py
@@ -90,7 +90,8 @@ if DATABASE_ENGINE == 'django.db.backends.mysql':
             'HOST': config('DATABASE_HOST', default='localhost'),
             'PORT': config('DATABASE_PORT', default='3306'),
             'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4',
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
             },
         }
     }
